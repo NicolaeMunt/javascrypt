afcaa7061dd70c76773e2d2d135cedf26d19f236
NicolaeMunt <114097090+NicolaeMunt@users.noreply.github.com>
2022-10-21 20:24:48 +0300

    Add files via upload

---
+++ javascrypt.js
@@ -0,0 +1,22 @@
+const text= 1;
+let text_1=true;
+// text="Salut";
+text_1="salut";
+let score= null;
+let a= 5;
+let plus='+';
+let b= 2;
+score=a**b
+let nume_prenume=null;
+let jojo="ahalaimahalai";
+let nume="Nicolae";
+let prenume="Munteanu";
+nume_prenume=nume+" "+prenume
+console.log(nume_prenume);
+
+console.log(typeof jojo);
+console.log(score);
+console.log(typeof text);
+console.log(text_1);
+// alert("jopajirnaia")
+// prompt("tu esti debil?")
